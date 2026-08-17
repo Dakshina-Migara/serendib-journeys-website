cce1a581dd04c9454c2039c0b2297c8b0d10dc2a
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-08-17 10:32:44 +0530

    refactor(StatCard): clean up code and use direct default export
    - Remove all comments and unnecessary blank lines to condense the file
    - Change component definition to use inline 'export default function' syntax

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from '../common/components/NavBar/NavBar';
+import Button from '../common/components/Button/Button';
+import StatCard from '../common/components/Statcard/StatCard';
 
 function App() {
   return (
     <>
-      <NavBar />
+      <StatCard />
     </>
   )
 }
